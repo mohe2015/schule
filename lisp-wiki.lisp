1ef18ed7a6da5b105ad7ac8f69fe0ec5979e6a32
mohe2015 <mohe2015@users.noreply.github.com>
2018-12-30 19:47:46 +0100

    Start the webserver

--- lisp-wiki.lisp
+++ lisp-wiki.lisp
@@ -3,3 +3,8 @@
   (:export))
 
 (in-package :lisp-wiki)
+; (stop *acceptor*)
+
+(DEFPARAMETER *acceptor* (make-instance 'easy-acceptor :port 8080 :document-root #p"/usr/share/nginx/html/www/"))
+(start *acceptor*)
+
