3309302886457d624a6ae5de8f6f3bcfa1930f33
mohe2015 <mohe2015@users.noreply.github.com>
2018-12-31 16:25:03 +0100

    Add initial database and wiki request handling

--- lisp-wiki.lisp
+++ lisp-wiki.lisp
@@ -4,9 +4,43 @@
   (:export))
 
 (in-package :lisp-wiki)
-; (stop *acceptor*)
+
+(defclass user (has-secure-password)
+  ((name :col-type (:varchar 60)
+	 :initarg :name
+	 :accessor user-name))
+  (:metaclass mito:dao-table-class))
+
+(mito:connect-toplevel :sqlite3 :database-name #P"/usr/share/nginx/html/database.db")
+
+(setf mito:*mito-logger-stream* t)
+
+(mito:ensure-table-exists 'user)
+
+(mito:create-dao 'user :name "Moritz Hedtke" :password "common-lisp")
+
+(defparameter *user* (mito:find-dao 'user :name "Moritz Hedtke"))
+
+(assert (auth *user* "common-lisp"))
+
+(assert (not (auth *user* "wrong-password")))
+
+;;(stop *acceptor*)
+
+(defvar *acceptor* nil)
 
 (if (not *acceptor*)
     (progn
       (defparameter *acceptor* (make-instance 'easy-acceptor :port 8080 :document-root #p"/usr/share/nginx/html/www/"))
       (start *acceptor*)))
+
+(define-easy-handler (api :uri "/api") ()
+  (setf (content-type*) "text/html")
+  "test")
+
+(push (create-prefix-dispatcher "/api/wiki" 'wiki-page) *dispatch-table*)
+
+(defun wiki-page ()
+  (SETF (header-out "X-LOL" *REPLY*) "TEST")
+  
+  (script-name* *REQUEST*))
