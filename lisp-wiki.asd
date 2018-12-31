8f8758f09cb3c24861c7deef4f1e178053d9bcb4
mohe2015 <mohe2015@users.noreply.github.com>
2018-12-31 15:53:42 +0100

    Some fixes

--- lisp-wiki.asd
+++ lisp-wiki.asd
@@ -1,3 +1,3 @@
 (defsystem :lisp-wiki
-  :depends-on (:cl-who :hunchentoot :parenscript)
+  :depends-on (:cl-who :hunchentoot :parenscript :mito :mito-attachment :mito-auth :can)
   :components ((:file "lisp-wiki")))
--- lisp-wiki.lisp
+++ lisp-wiki.lisp
@@ -1,10 +1,12 @@
 (defpackage :lisp-wiki
-  (:use :common-lisp :cl-who :hunchentoot :parenscript)
+  (:use :common-lisp :cl-who :hunchentoot :parenscript :mito :mito-attachment :mito-auth :can)
+  (:shadowing-import-from :mito-attachment :content-type)
   (:export))
 
 (in-package :lisp-wiki)
 ; (stop *acceptor*)
 
-(DEFPARAMETER *acceptor* (make-instance 'easy-acceptor :port 8080 :document-root #p"/usr/share/nginx/html/www/"))
-(start *acceptor*)
-
+(if (not *acceptor*)
+    (progn
+      (defparameter *acceptor* (make-instance 'easy-acceptor :port 8080 :document-root #p"/usr/share/nginx/html/www/"))
+      (start *acceptor*)))
