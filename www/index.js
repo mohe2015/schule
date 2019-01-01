3b948593b3eec4cecaeafbce6d1ace6264f05d89
mohe2015 <mohe2015@users.noreply.github.com>
2019-01-01 18:48:29 +0100

    Format javascript

--- www/index.js
+++ www/index.js
@@ -1,98 +1,86 @@
 /*eslint-env browser, jquery*/
 $(document).ready(function() {
 
-    var FinishedButton = function (context) {
-      var ui = $.summernote.ui;
-      var button = ui.button({
-        contents: '<i class="fa fa-check"/>',
-        tooltip: 'Fertig',
-        click: function () {
-            
-            $('#publish-changes-modal').on('shown.bs.modal', function () {
-  $('#change-summary').trigger('focus')
-});
-            
-            // modal 1050
-            // modal backdrop: 1040
-            $('#publish-changes-modal').modal('show');
-            
-            
-         // $('article').summernote('destroy');
-        //  $('.tooltip').hide();
-        }
-      });
-      return button.render();
+    var FinishedButton = function(context) {
+        var ui = $.summernote.ui;
+        var button = ui.button({
+            contents: '<i class="fa fa-check"/>',
+            tooltip: 'Fertig',
+            click: function() {
+
+                $('#publish-changes-modal').on('shown.bs.modal', function() {
+                    $('#change-summary').trigger('focus')
+                });
+
+                // modal 1050
+                // modal backdrop: 1040
+                $('#publish-changes-modal').modal('show');
+
+
+                // $('article').summernote('destroy');
+                //  $('.tooltip').hide();
+            }
+        });
+        return button.render();
     }
 
-    var CancelButton = function (context) {
-      var ui = $.summernote.ui;
-      var button = ui.button({
-        contents: '<i class="fa fa-times"/>',
-        tooltip: 'Abbrechen',
-        click: function () {
-            $('article').summernote('destroy');
-            $('.tooltip').hide();
-        }
-      });
+    var CancelButton = function(context) {
+        var ui = $.summernote.ui;
+        var button = ui.button({
+            contents: '<i class="fa fa-times"/>',
+            tooltip: 'Abbrechen',
+            click: function() {
+                $('article').summernote('destroy');
+                $('.tooltip').hide();
+            }
+        });
 
-      return button.render();
+        return button.render();
     }
 
     $('#publish-changes').click(function() {
         $('#publish-changes').hide();
-       $('#publishing-changes').show();
-        
-          $.post("api/wiki/Startseite", function (data) {
-        $('article').html( data );
-       
-       $('#loading').fadeOut('slow');
-      $('#page').fadeIn('slow');
-       
-   })
-    .fail(function() {
-      alert("Fehler beim Laden des Artikels!");
-   });
-    
-        
+        $('#publishing-changes').show();
+
+        $.post("api/wiki/Startseite", function(data) {
+            $('article').html(data);
+
+            $('#loading').fadeOut('slow');
+            $('#page').fadeIn('slow');
+
+        })
+        .fail(function() {
+            alert("Fehler beim Laden des Artikels!");
+        });
     });
-    
-        
+
+
     $(".edit-button").click(function() {
         $('article').summernote({
-          dialogsFade: true,
-          focus: true,
-          buttons: {
-            finished: FinishedButton,
-              cancel: CancelButton
-          },
-          toolbar: [
-            ['style', ['bold', 'italic', 'underline', 'superscript', 'subscript']],
-            ['para', ['ul', 'ol', 'indent', 'outdent',  'justifyLeft', 'justifyCenter' ]],
-            ['insert', ['link', 'picture', 'video', 'table']],
-            ['management', ['undo', 'redo', 'help', 'cancel', 'finished']]
-          ]
+            dialogsFade: true,
+            focus: true,
+            buttons: {
+                finished: FinishedButton,
+                cancel: CancelButton
+            },
+            toolbar: [
+                ['style', ['bold', 'italic', 'underline', 'superscript', 'subscript']],
+                ['para', ['ul', 'ol', 'indent', 'outdent', 'justifyLeft', 'justifyCenter']],
+                ['insert', ['link', 'picture', 'video', 'table']],
+                ['management', ['undo', 'redo', 'help', 'cancel', 'finished']]
+            ]
         });
         $('article').summernote('fullscreen.toggle');
     });
-    
-    
-   // $('.note-status-output').first().html(
-//  '<div class="alert alert-danger">' +
-//    'This is an error using a Bootstrap alert that has been restyled to fit here.' +
- // '</div>'
-//);
 
-   $.get("api/wiki/Startseite", function (data) {
-        $('article').html( data );
-       
-       $('#loading').fadeOut('slow');
-      $('#page').fadeIn('slow');
-       
-   })
+    $.get("api/wiki/Startseite", function(data) {
+        $('article').html(data);
+
+        $('#loading').fadeOut('slow');
+        $('#page').fadeIn('slow');
+
+    })
     .fail(function() {
-      alert("Fehler beim Laden des Artikels!");
-   });
-    
-    
-    
+        alert("Fehler beim Laden des Artikels!");
+    });
 });
