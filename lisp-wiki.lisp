880c8f2e26f00a53f1d3f992e9fc1ac156e9ece3
mohe2015 <mohe2015@users.noreply.github.com>
2019-01-01 16:51:48 +0100

    Small summernote improvements

--- lisp-wiki.lisp
+++ lisp-wiki.lisp
@@ -5,17 +5,33 @@
 
 (in-package :lisp-wiki)
 
-(defclass user (has-secure-password)
-  ((name :col-type (:varchar 60)
-	 :initarg :name
-	 :accessor user-name))
-  (:metaclass mito:dao-table-class))
 
 (mito:connect-toplevel :sqlite3 :database-name #P"/usr/share/nginx/html/database.db")
 
+(deftable user (has-secure-password)
+  ((name :col-type (:varchar 60))))
+
+(deftable wiki-article ()
+  ((title :col-type (:varchar 128))))
+
+(deftable wiki-article-revision ()
+  ((user :col-type user)
+   (wiki-article :col-type wiki-article)
+   (content :col-type (:blob))))
+
+
 (setf mito:*mito-logger-stream* t)
 
-(mito:ensure-table-exists 'user)
+(ensure-table-exists 'user)
+(ensure-table-exists 'wiki-article)
+(ensure-table-exists 'wiki-article-revision)
+(migrate-table 'user)
+(migrate-table 'wiki-article)
+(migrate-table 'wiki-article-revision)
+
+;; (create-dao 'wiki-article :title "Startseite")
+;;  (create-dao 'wiki-article-revision :author *user* :article (find-dao 'wiki-article :title "Startseite") :content "THIS IS THE INITIAL CONTENT")
+
 
 (mito:create-dao 'user :name "Moritz Hedtke" :password "common-lisp")
 
--- www/index.js
+++ www/index.js
@@ -30,81 +30,26 @@ $(document).ready(function() {
       return button.render();
     }
 
-    
-    (function(factory) {
-  /* global define */
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define(['jquery'], factory);
-  } else if (typeof module === 'object' && module.exports) {
-    // Node/CommonJS
-    module.exports = factory(require('jquery'));
-  } else {
-    // Browser globals
-    factory(window.jQuery);
-  }
-}(function($) {
-  // Extends plugins for adding hello.
-  //  - plugin is external module for customizing.
-  $.extend($.summernote.plugins, {
-    /**
-     * @param {Object} context - context object has status of editor.
-     */
-    'hello': function(context) {
-      var self = this;
-
-      // ui has renders to build ui elements.
-      //  - you can create a button with `ui.button`
-      var ui = $.summernote.ui;
-
-     
-      // This method will be called when editor is initialized by $('..').summernote();
-      // You can create elements for plugin
-      this.initialize = function() {
-        // Find editor
-            var $editor = $('.note-editor'),
-                $toolbar = $editor.find('.note-toolbar');
-            // Scrolling event
-            var repositionToolbar = function() {
-                var windowTop = $(window).scrollTop(),
-                    editorTop = $editor.offset().top,
-                    editorBottom = editorTop + $editor.height();
-                if (windowTop > editorTop && windowTop < editorBottom) {
-                    $toolbar.css('position', 'fixed');
-                    $toolbar.css('top', '0px');
-                    $toolbar.css('width', $editor.width() + 'px');
-                    $toolbar.css('z-index', '1039');
-                    $editor.css('padding-top', '42px');
-                }
-                else {
-                    $toolbar.css('position', 'static');
-                    $editor.css('padding-top', '0px');
-                }
-            };
-            // Move it
-            $(window).scroll(repositionToolbar);
-            repositionToolbar();
-      };
-
-    },
-  });
-}));    
         
     $("#edit-button").click(function() {
         $("#edit-button").hide();
-$('article').summernote({
-  buttons: {
-    finished: FinishedButton,
-      cancel: CancelButton
-  },
-  toolbar: [
-    ['style', ['bold', 'italic', 'underline', 'superscript', 'subscript']],
-    ['para', ['ul', 'ol', 'indent', 'outdent',  'justifyLeft', 'justifyCenter' ]],
-    ['insert', ['link', 'picture', 'video', 'table']],
-    ['management', ['undo', 'redo', 'help', 'cancel', 'finished']]
-  ]
-});
-});
+        $('article').summernote({
+          dialogsFade: true,
+          focus: true,
+          buttons: {
+            finished: FinishedButton,
+              cancel: CancelButton
+          },
+          toolbar: [
+            ['style', ['bold', 'italic', 'underline', 'superscript', 'subscript']],
+            ['para', ['ul', 'ol', 'indent', 'outdent',  'justifyLeft', 'justifyCenter' ]],
+            ['insert', ['link', 'picture', 'video', 'table']],
+            ['management', ['undo', 'redo', 'help', 'cancel', 'finished']]
+          ]
+        });
+        $('article').summernote('fullscreen.toggle');
+
+    });
     
     
    // $('.note-status-output').first().html(
