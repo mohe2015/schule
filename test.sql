18642d9a7ca897ad4d29267e05fc0a87db9e9ccf
mohe2015 <mohe2015@users.noreply.github.com>
2019-01-08 23:30:20 +0100

    Start implementing better search

--- test.sql
+++ test.sql
@@ -1,17 +1,1 @@
-SELECT a.title, ts_rank_cd(textsearch, query) AS rank FROM wiki_article AS A, to_tsquery('Elefant') query, to_tsvector((SELECT content FROM wiki_article_revision WHERE article_id = a.id ORDER BY id DESC LIMIT 1)) textsearch WHERE query @@ textsearch ORDER BY rank DESC;
-
-
-
-SELECT a.title, ts_rank_cd(textsearch, query) AS rank FROM wiki_article AS A, to_tsquery('Elefant') query, (setweight(to_tsvector(a.title), 'A') || setweight(to_tsvector((SELECT content FROM wiki_article_revision WHERE article_id = a.id ORDER BY id DESC LIMIT 1)), 'D')) textsearch WHERE query @@ textsearch ORDER BY rank DESC;
-
-
-
-
-SELECT a.title, ts_rank_cd((setweight(to_tsvector(a.title), 'A') || setweight(to_tsvector((SELECT content FROM wiki_article_revision WHERE article_id = a.id ORDER BY id DESC LIMIT 1)), 'D')), query) AS rank FROM wiki_article AS A, to_tsquery('Elefant') query WHERE query @@ (setweight(to_tsvector(a.title), 'A') || setweight(to_tsvector((SELECT content FROM wiki_article_revision WHERE article_id = a.id ORDER BY id DESC LIMIT 1)), 'D')) ORDER BY rank DESC;
-
-
-
-
-SELECT a.title, ts_rank_cd((setweight(to_tsvector(a.title), 'A') || setweight(to_tsvector((SELECT content FROM wiki_article_revision WHERE article_id = a.id ORDER BY id DESC LIMIT 1)), 'D')), query) AS rank, ts_headline(a.title || (SELECT content FROM wiki_article_revision WHERE article_id = a.id ORDER BY id DESC LIMIT 1), websearch_to_tsquery('Elefant')) FROM wiki_article AS A, websearch_to_tsquery('Elefant') query WHERE query @@ (setweight(to_tsvector(a.title), 'A') || setweight(to_tsvector((SELECT content FROM wiki_article_revision WHERE article_id = a.id ORDER BY id DESC LIMIT 1)), 'D')) ORDER BY rank DESC;
-
 SELECT a.title, ts_rank_cd((setweight(to_tsvector(a.title), 'A') || setweight(to_tsvector((SELECT content FROM wiki_article_revision WHERE article_id = a.id ORDER BY id DESC LIMIT 1)), 'D')), query) AS rank, ts_headline(a.title || (SELECT content FROM wiki_article_revision WHERE article_id = a.id ORDER BY id DESC LIMIT 1), websearch_to_tsquery('Elefant')) FROM wiki_article AS A, websearch_to_tsquery('Elefant') query WHERE query @@ (setweight(to_tsvector(a.title), 'A') || setweight(to_tsvector((SELECT content FROM wiki_article_revision WHERE article_id = a.id ORDER BY id DESC LIMIT 1)), 'D')) ORDER BY rank DESC;
