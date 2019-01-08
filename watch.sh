22b0f3e1b49824e787aeaff3ece00f94c8efa9e0
mohe2015 <mohe2015@users.noreply.github.com>
2019-01-08 15:17:06 +0100

    Small update

--- watch.sh
+++ watch.sh
@@ -2,11 +2,24 @@
 
 update_hash () {
 	hash=($(sha512sum www/$1))
-	sed -r 's/'$1'\?v=[a-f0-9]+/'$1'\?v='$hash'/g' www/index.html
+	sed -r -i 's/'$1'\?v=[a-f0-9]+/'$1'\?v='$hash'/g' www/index.html
 }
 
 update_hashes () {
+	update_hash 'bootstrap.css'
+	update_hash 'all.css'
+	update_hash 'summernote-bs4.css'
+	update_hash 'index.css'
+	update_hash 'mathlive.core.css'
+	update_hash 'mathlive.css'
+	update_hash 'jquery-3.3.1.js'
+	update_hash 'popper.js'
+	update_hash 'bootstrap.js'
+	update_hash 'summernote-bs4.js'
+	update_hash 'visual-diff.js'
 	update_hash 'index.js'
+	update_hash 'mathlive.js'
+	update_hash 'summernote-math.js'
 }
 
 update_hashes
