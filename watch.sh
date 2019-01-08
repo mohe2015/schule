45eba11f0713301dcb98b2202ac9d02a1ef3c1d3
mohe2015 <mohe2015@users.noreply.github.com>
2019-01-08 15:13:26 +0100

    Write a watch script

--- watch.sh
+++ watch.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+update_hash () {
+	hash=($(sha512sum www/$1))
+	sed -r 's/'$1'\?v=[a-f0-9]+/'$1'\?v='$hash'/g' www/index.html
+}
+
 while true; do
 
-inotifywait --event modify -r www/ --exclude '.*kate-swp' && echo "Change detected"
+inotifywait --event modify -r www/ --exclude '.*kate-swp'
+update_hash 'index.js'
 
 done
