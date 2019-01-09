61edbae917a6661d6115f92f5a646b450a616b91
mohe2015 <mohe2015@users.noreply.github.com>
2019-01-09 18:37:56 +0100

    Lots of improvement

--- test/tsquery-converter.lisp
+++ test/tsquery-converter.lisp
@@ -6,9 +6,13 @@
 
 (defparameter *query* (dbi:prepare *connection* "SELECT to_tsquery('german', ?), to_tsquery('german', ?) @@ to_tsvector('german', 'Dies ist ein sehr toller Text über Elefanten. Sie können laufen, rennen und trompeten');"))
 
+(trace tsquery-convert)
+(trace tsquery-convert-part)
+(trace handle-quoted)
+(trace handle-unquoted)
+
 (defun test (query)
   (let ((query (tsquery-convert query)))
-    (print query)
     (dbi:fetch-all (dbi:execute *query* query query))))
 
 (defun handle-quoted (query)
@@ -17,15 +21,9 @@
 (defun handle-unquoted (query)
   (concat "(" (join " & " (split " " query)) ")"))
 
-(defun tsquery-convert (query)
-  ;; if query contains an odd amount of qoutes add one at the end
-  (if (oddp (count #\" query))
-      (setf query (concatenate 'string query "\"")))
-
-  ;; split at quotes
+(defun tsquery-convert-part (query)
+   ;; split at quotes
   (setf query (split #\" query))
-
-  (print query)
 
   ;; handle quoted and unquoted parts separately
   (setf query
@@ -37,13 +35,27 @@
 	     (setf e (handle-unquoted e))
 	   else do
 	     (setf e (handle-quoted e))
-	     collect e))
+	   collect e))
 
+  (setf query (join " & " query))
+
+  query)
+
+(defun tsquery-convert (query)
+  ;; if query contains an odd amount of qoutes add one at the end
+  (if (oddp (count #\" query))
+      (setf query (concatenate 'string query "\"")))
+  
+  ;; split at OR
+  (setf query (split "OR" query))
+
+  (setf query (loop for e in query
+		   collect (tsquery-convert-part (trim e))))
+
+  
   ;; handle logical not operator
 
-
-
   ;; join substrings
-  (setf query (join " & " query))
+  (setf query (join " | " query))
   
   query)
