9aa2aa9aa2b26e01e7bc502ff661edc03d96adb7
mohe2015 <mohe2015@users.noreply.github.com>
2019-01-10 13:50:20 +0100

    Bad working cleaning script

--- www/index.js
+++ www/index.js
@@ -211,7 +211,21 @@ $(document).ready(function() {
                 ['para', ['ul', 'ol', 'indent', 'outdent']],
                 ['insert', ['link', 'picture', 'video', 'table', 'math']],
                 ['management', ['undo', 'redo', 'help', 'cancel', 'finished']]
-            ]
+            ],
+            cleaner:{
+              action: 'paste',
+              newline: '<br>',
+              notStyle: 'position:absolute;top:0;left:0;right:0',
+              icon: '<i class="note-icon">[Your Button]</i>',
+              keepHtml: false, // Remove all Html formats
+              keepOnlyTags: ['<p>', '<br>', '<ul>', '<li>', '<b>', '<strong>','<i>', '<a>'], // If keepHtml is true, remove all tags except these
+              keepClasses: false, // Remove Classes
+              badTags: ['style', 'script', 'applet', 'embed', 'noframes', 'noscript', 'html'], // Remove full tags with contents
+              badAttributes: ['style', 'start'], // Remove attributes from remaining tags
+              limitChars: false, // 0/false|# 0/false disables option
+              limitDisplay: 'both', // text|html|both
+              limitStop: false // true/false
+            }
         });
         setFullscreen(true);
     }
