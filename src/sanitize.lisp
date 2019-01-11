2cd6348a2f0a52951805cd8f8a2dc26c80969a10
mohe2015 <mohe2015@users.noreply.github.com>
2019-01-11 23:47:27 +0100

    Fix #70

--- src/sanitize.lisp
+++ src/sanitize.lisp
@@ -2,6 +2,8 @@
 
 (define-sanitize-mode *sanitize-spickipedia*
     :elements ("h1" "h2" "h3" "h4" "h5" "h6" "p" "strike" "sub" "b" "u" "i" "sup" "table" "tbody" "tr" "td" "ul" "a" "br" "ol" "li" "img" "iframe" "span")
+
+    :add-attributes (("a"      . (("rel" . "noopener noreferrer"))))
     
     :attributes (("h1"          . ("align" "style"))
 		 ("span"        . ("class"))
