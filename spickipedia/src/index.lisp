dcf0a3f05868397a52c8b28e4d89cfdfb8908194
mohe2015 <mohe2015@users.noreply.github.com>
2019-01-19 16:24:46 +0100

    Start implementing javascript in lisp

--- spickipedia/src/index.lisp
+++ spickipedia/src/index.lisp
@@ -1,2 +1,26 @@
-(setf (@ window onerror) (lambda (message source lineno colno error)
-			   (alert "Es ist ein Fehler aufgetreten! Melde ihn bitte dem Entwickler! ")))
+(setf (chain window onerror) (lambda (message source lineno colno error)
+			   (alert (concatenate 'string "Es ist ein Fehler aufgetreten! Melde ihn bitte dem Entwickler! " message " source: " source " lineno: " lineno " colno: " colno " error: " error))))
+
+(chain
+ ($ "body")
+ (on "click" ".history-pushState"
+     (lambda (e)
+       (chain e (prevent-default))
+       (chain window history (push-state nil nil (chain ($ this) (data "href"))))
+       (update-state)
+       F)))
+
+(chain
+ ($ "#refresh")
+ (click (lambda (e)
+	  (chain e (prevent-default))
+	  (update-state)
+	  F)))
+
+(defun handle-error (thrown-error show-error-page)
+  (if (= thrown-error "Authorization Required")
+      (let ((name (chain ($ "#inputName") (val (chain window local-storage name))))
+	    (state (create last-url (chain window location href))))
+	(chain window local-storage (remove-item "name"))
+	(chain window history (push-state state) nil "/login")
+	(update-state))))
