30438f8d5b9a4d35f02a81da661d7a2be123ec68
mohe2015 <mohe2015@users.noreply.github.com>
2019-01-20 18:32:28 +0100

    Add read-cookie

--- spickipedia/src/index.lisp
+++ spickipedia/src/index.lisp
@@ -96,3 +96,10 @@
 		 (chain ($ "#article-link-title") (trigger "focus")))))
 	  (chain ($ "#spickiLinkModal") (modal "show"))))))
      (render))))
+
+(defun read-cookie (name)
+  (let ((name-eq (concatenate 'string name "="))
+	(ca (chain document cookie (split ";"))))
+    (loop for c in ca do
+	 (if (chain c (trim) (starts-with name-eq))
+	     (return (chain c (substring (chain name-eq length))))))))
