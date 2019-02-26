8bd0d5f5a67bca057267047fbfd0b7226a091576
mohe2015 <mohe2015@users.noreply.github.com>
2019-02-26 19:22:46 +0100

    Little bit of cleanup

--- spickipedia/js/0_categories.lisp
+++ spickipedia/js/0_categories.lisp
@@ -9,6 +9,14 @@
       (who-ps-html
        (:span :class "closable-badge"
 	      (:span :class "closable-badge-label" (chain ($ "#new-category") (val)))
-	      (:button :type "button" :class "close" :aria-label "Close"
+	      (:button :type "button" :class "close close-tag" :aria-label "Close"
 		       (:span :aria-hidden "true" "&times;"))))))
     (chain ($ "#new-category") (val "")))))
+
+(chain
+ ($ "body")
+ (on
+  "click"
+  ".close-tag"
+  (lambda (e)
+    (chain ($ this) (parent) (remove)))))
