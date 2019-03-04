f9495605d59e5be562b8c253d1a094458fa11925
mohe2015 <mohe2015@users.noreply.github.com>
2019-03-04 22:25:05 +0100

    Small fix

--- spickipedia/src/db.lisp
+++ spickipedia/src/db.lisp
@@ -50,80 +50,81 @@
   `(let ((*connection* ,conn))
      ,@body))
 
+(defclass user ()
+  ((name  :col-type (:varchar 64)
+    :initarg :name
+    :accessor user-name)
+    (group :col-type (:varchar 64)
+    :initarg :group
+    :inflate (compose #'make-keyword #'string-upcase)
+          :deflate #'string-downcase
+    :accessor user-group)
+    (hash  :col-type (:varchar 512)
+    :initarg :hash
+    :accessor user-hash))
+  (:metaclass mito:dao-table-class))
+
+(defclass wiki-article ()
+  ((title :col-type (:varchar 128)
+    :initarg :title
+    :accessor wiki-article-title))
+  (:metaclass mito:dao-table-class))
+
+(defclass wiki-article-revision ()
+  ((author :col-type user
+      :initarg :author
+      :accessor wiki-article-revision-author)
+    (article :col-type wiki-article
+:initarg :article
+:accessor wiki-article-revision-article)
+    (summary :col-type (:varchar 256)
+:initarg :summary
+:accessor wiki-article-revision-summary)
+    (content :col-type (:text)
+:initarg :content
+:accessor wiki-article-revision-content))
+  (:metaclass mito:dao-table-class))
+
+(defclass wiki-article-revision-category ()
+  ((revision :col-type wiki-article-revision
+  :initarg :revision
+  :accessor wiki-article-revision-category-revision)
+    (category :col-type (:varchar 256)
+  :initarg :category
+  :accessor wiki-article-revision-category-category))
+  (:metaclass mito:dao-table-class))
+
+(defclass my-session ()
+  ((session-cookie :col-type (:varchar 512)
+        :initarg :session-cookie
+        :accessor my-session-cookie)
+    (csrf-token     :col-type (:varchar 512)
+        :initarg :csrf-token
+        :accessor my-session-csrf-token)
+    (user           :col-type (or user :null)
+        :initarg  :user
+        :accessor my-session-user))
+  (:metaclass mito:dao-table-class))
+
+(defclass quiz ()
+  ()
+  (:metaclass mito:dao-table-class))
+
+(defclass quiz-revision ()
+  ((author :col-type user
+      :initarg :author
+      :accessor quiz-revision-author)
+    (quiz :col-type quiz
+    :initarg :quiz
+    :accessor quiz-revision-quiz)
+    (content :col-type (:text)
+:initarg :content
+:accessor quiz-revision-content))
+  (:metaclass mito:dao-table-class))
+    
+     
 (defun setup-db ()
   (with-connection (db)
-    (defclass user ()
-      ((name  :col-type (:varchar 64)
-	      :initarg :name
-	      :accessor user-name)
-       (group :col-type (:varchar 64)
-	      :initarg :group
-	      :inflate (compose #'make-keyword #'string-upcase)
-              :deflate #'string-downcase
-	      :accessor user-group)
-       (hash  :col-type (:varchar 512)
-	      :initarg :hash
-	      :accessor user-hash))
-      (:metaclass mito:dao-table-class))
-
-    (defclass wiki-article ()
-      ((title :col-type (:varchar 128)
-	      :initarg :title
-	      :accessor wiki-article-title))
-      (:metaclass mito:dao-table-class))
-
-    (defclass wiki-article-revision ()
-      ((author :col-type user
-	       :initarg :author
-	       :accessor wiki-article-revision-author)
-       (article :col-type wiki-article
-		:initarg :article
-		:accessor wiki-article-revision-article)
-       (summary :col-type (:varchar 256)
-		:initarg :summary
-		:accessor wiki-article-revision-summary)
-       (content :col-type (:text)
-		:initarg :content
-		:accessor wiki-article-revision-content))
-      (:metaclass mito:dao-table-class))
-
-    (defclass wiki-article-revision-category ()
-      ((revision :col-type wiki-article-revision
-		 :initarg :revision
-		 :accessor wiki-article-revision-category-revision)
-       (category :col-type (:varchar 256)
-		 :initarg :category
-		 :accessor wiki-article-revision-category-category))
-      (:metaclass mito:dao-table-class))
-
-    (defclass my-session ()
-      ((session-cookie :col-type (:varchar 512)
-		       :initarg :session-cookie
-		       :accessor my-session-cookie)
-       (csrf-token     :col-type (:varchar 512)
-		       :initarg :csrf-token
-		       :accessor my-session-csrf-token)
-       (user           :col-type (or user :null)
-		       :initarg  :user
-		       :accessor my-session-user))
-      (:metaclass mito:dao-table-class))
-
-    (defclass quiz ()
-      ()
-      (:metaclass mito:dao-table-class))
-
-    (defclass quiz-revision ()
-      ((author :col-type user
-	       :initarg :author
-	       :accessor quiz-revision-author)
-       (quiz :col-type quiz
-	     :initarg :quiz
-	     :accessor quiz-revision-quiz)
-       (content :col-type (:text)
-		:initarg :content
-		:accessor quiz-revision-content))
-      (:metaclass mito:dao-table-class))
-
     (mito:ensure-table-exists 'user)
     (mito:ensure-table-exists 'wiki-article)
     (mito:ensure-table-exists 'wiki-article-revision)
