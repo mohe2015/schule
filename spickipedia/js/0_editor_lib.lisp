a604a6abca87b60edcac7278a9feff74731b172d
mohe2015 <mohe2015@users.noreply.github.com>
2019-03-09 11:13:52 +0100

    Use macro and small fixes

--- spickipedia/js/0_editor_lib.lisp
+++ spickipedia/js/0_editor_lib.lisp
@@ -4,15 +4,18 @@
 (setf (chain document (get-elements-by-tag-name "article") 0 content-editable) T)
 
 (defmacro tool (id &body body)
-    nil)
+  `(chain
+   document
+   (get-element-by-id ,id)
+   (add-event-listener
+    "click"
+    (lambda (event)
+      (chain event (prevent-default))
+      ,@body
+      F))))
 
-(chain
- document
- (get-element-by-id "format-p")
- (add-event-listener
-  "click"
-  (lambda (event)
-    (chain event (prevent-default))
-    (chain document (get-elements-by-tag-name "article") 0 (focus))
-    (chain document (exec-command "formatBlock" F "<p>"))
-    F)))
+(tool "format-p"
+    (chain document (exec-command "formatBlock" F "<p>")))
+
+(tool "format-h2"
+      (chain document (exec-command "formatBlock" F "<h2>")))
