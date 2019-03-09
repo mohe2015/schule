362e5c68b22785305369ce01ee1206cb03765a93
mohe2015 <mohe2015@users.noreply.github.com>
2019-03-09 11:24:40 +0100

    Implement all basic tools

--- spickipedia/js/0_editor_lib.lisp
+++ spickipedia/js/0_editor_lib.lisp
@@ -12,10 +12,36 @@
     (lambda (event)
       (chain event (prevent-default))
       ,@body
+      (chain document (get-elements-by-tag-name "article") 0 (focus))
       F))))
+
+(defmacro stool (id)
+  `(tool ,id
+	 (chain document (exec-command ,id F))))
 
 (tool "format-p"
     (chain document (exec-command "formatBlock" F "<p>")))
 
 (tool "format-h2"
       (chain document (exec-command "formatBlock" F "<h2>")))
+
+(tool "format-h3"
+      (chain document (exec-command "formatBlock" F "<h3>")))
+
+(stool "superscript")
+(stool "subscript")
+(stool "insertUnorderedList")
+(stool "insertOrderedList")
+(stool "indent")
+(stool "outdent")
+
+;; TODO link
+;; TODO image
+;; TODO table
+;; TODO formula
+
+(stool "undo")
+(stool "redo")
+
+;; TODO settings
+;; TODO finish
