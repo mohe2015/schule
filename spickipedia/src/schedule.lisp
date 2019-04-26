d833c9afd647d39888caa67e80d415a43134e956
mohe2015 <Moritz.Hedtke@t-online.de>
2019-04-26 22:16:49 +0200

    Fix defroute

--- spickipedia/src/schedule.lisp
+++ spickipedia/src/schedule.lisp
@@ -1,11 +1,10 @@
 (in-package :spickipedia.web)
 
 (my-defroute :POST "/api/teachers" (:admin :user) (|name| |initial|) "text/html"
-  (format t "data: ~a ~a ~a" user |name| |initial|)
   (let* ((teacher  (create-dao 'teacher))
          (revision (create-dao 'teacher-revision
                                :author user
                                :teacher teacher
-                               :name |name|
-                               :initial |initial|)))
+                               :name (first |name|)
+                               :initial (first |initial|))))
     (format nil "~a" (object-id teacher))))
