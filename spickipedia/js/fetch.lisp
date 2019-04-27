fc890198d7ff8db7b2420f7337b0714026a69b1a
mohe2015 <Moritz.Hedtke@t-online.de>
2019-04-27 14:57:44 +0200

    Throw error correctly

--- spickipedia/js/fetch.lisp
+++ spickipedia/js/fetch.lisp
@@ -2,14 +2,16 @@
 
 (export
   (defun handle-fetch-error (error)
-    (chain console (log error))
+    (chain console (log (chain error response)))
     (alert "error")))
 
 (export
   (defun check-status (response)
     (if (and (>= (chain response status) 200) (< (chain response status) 300))
       (chain -Promise (resolve response))
-      (chain -promise (reject (new (-error (chain response status-text))))))))
+      (let ((error (new (-error (chain response status-text)))))
+        (setf (chain error response) response)
+        (throw error)))))
 
 (export
   (defun json (response)
