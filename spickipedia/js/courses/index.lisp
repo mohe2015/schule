e5945a6645dd4b6883e1dbe9f1b2ab23aae6f6c3
mohe2015 <Moritz.Hedtke@t-online.de>
2019-05-18 19:35:00 +0200

    Remove logging

--- spickipedia/js/courses/index.lisp
+++ spickipedia/js/courses/index.lisp
@@ -21,7 +21,6 @@
           (setf (chain courses-list inner-h-t-m-l) "")
           (loop for page in data do
               (let ((template (get-template "courses-list-html")))
-                (chain console (log (chain page name)))
                 (setf (chain template (query-selector ".courses-list-subject") inner-text) (chain page subject))
                 (chain document (get-element-by-id "courses-list") (append template)))))))
     (catch handle-fetch-error)))
