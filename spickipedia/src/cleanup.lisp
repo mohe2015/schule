c1eeac98932d85ff5648596f44fe809cf67c9543
mohe2015 <Moritz.Hedtke@t-online.de>
2019-07-05 13:35:14 +0200

    Update

--- spickipedia/src/cleanup.lisp
+++ spickipedia/src/cleanup.lisp
@@ -1,9 +1,26 @@
+(defun mapc-directory-tree (fn directory &key (depth-first-p t))
+  (dolist (entry (cl-fad:list-directory directory))
+    (unless depth-first-p
+      (funcall fn entry))
+    (when (cl-fad:directory-pathname-p entry)
+      (mapc-directory-tree fn entry))
+    (when depth-first-p
+      (funcall fn entry))))
 
+(defun update-file (file)
+  (if (pathname-name file)
+    (let ((result
+            (with-open-file (s file)
+              (macrolet ((:div (&rest rest)
+                           (if (equal (subseq rest 0 5)
+                                      '(:style "display: none;" :class "container-fluid my-tab position-absolute" :id))
+                             ``(tab ,,(nth 5 rest) ,',@(subseq rest 6))
+                             nil)))
+                (loop for sexp = (read s)
+                      while sexp
+                      collect sexp)))))
+      (with-open-file (s file) :direction :output :if-exists :supersede
+        (loop for sexp in result
+          (print sexp s))))))
 
-(macrolet ((:div (&rest rest)
-             (if (equal (subseq rest 0 5)
-                        '(:style "display: none;" :class "container-fluid my-tab position-absolute" :id))
-               ``(tab ,,(nth 5 rest) ,',@(subseq rest 6))
-               nil)))
-  (:div :style "display: none;" :class "container-fluid my-tab position-absolute" :id "create-teacher-tab"
-    (:FORM :method "POST" :action "/api/teachers" :id "create-teacher-form")))
+(mapc-directory-tree update-file (asdf:system-source-directory :spickipedia))
