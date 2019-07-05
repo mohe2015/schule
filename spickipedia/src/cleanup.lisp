4c9598243d24fa21bd91ea9565202e23ab118049
mohe2015 <Moritz.Hedtke@t-online.de>
2019-07-05 12:38:10 +0200

    Initial idea

---
+++ spickipedia/src/cleanup.lisp
@@ -0,0 +1,9 @@
+
+
+(macrolet ((:div (&rest rest)
+             (if (equal (subseq rest 0 5)
+                        '(:style "display: none;" :class "container-fluid my-tab position-absolute" :id))
+               ``(tab ,,(nth 5 rest))
+               nil)))
+  (:div :style "display: none;" :class "container-fluid my-tab position-absolute" :id "create-teacher-tab"
+    (:FORM :method "POST" :action "/api/teachers" :id "create-teacher-form")))
