95166b70c21bd1e2660533dfe54a52ccc56e6bb1
mohe2015 <Moritz.Hedtke@t-online.de>
2019-07-05 13:15:22 +0200

    Hopefully that is working

--- spickipedia/src/cleanup.lisp
+++ spickipedia/src/cleanup.lisp
@@ -3,7 +3,7 @@
 (macrolet ((:div (&rest rest)
              (if (equal (subseq rest 0 5)
                         '(:style "display: none;" :class "container-fluid my-tab position-absolute" :id))
-               ``(tab ,,(nth 5 rest))
+               ``(tab ,,(nth 5 rest) ,',@(subseq rest 6))
                nil)))
   (:div :style "display: none;" :class "container-fluid my-tab position-absolute" :id "create-teacher-tab"
     (:FORM :method "POST" :action "/api/teachers" :id "create-teacher-form")))
