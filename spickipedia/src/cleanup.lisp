739c807630814e7ab4bc6952151bf91b4efa26f7
mohe2015 <Moritz.Hedtke@t-online.de>
2019-07-05 16:22:51 +0200

    Update cleanup.lisp

--- spickipedia/src/cleanup.lisp
+++ spickipedia/src/cleanup.lisp
@@ -8,42 +8,27 @@
     (when (directory-pathname-p entry) (mapc-directory-tree fn entry))
     (when depth-first-p (funcall fn entry))))
 (defun update-file (file)
-  (if (and (pathname-name file)
-           (str:ends-with? ".lisp" (file-namestring file)))
-      (let ((result
-             (with-open-file (s file)
-               (loop for sexp = (macrolet ((:div (&rest rest)
-                                             (if (equal (subseq rest 0 5)
-                                                        '(:style
-                                                          "display: none;"
-                                                          :class
-                                                          "container-fluid my-tab position-absolute"
-                                                          :id))
-                                                 ``(tab ,,(nth 5 rest)
-                                                    ,',@(subseq rest 6))
-                                                 (print rest))))
-                                  (print (read s nil)))
-                     while sexp
-                     collect sexp))))
-        (with-open-file (s file :direction :output :if-exists :supersede)
-          (let ((*print-case* :downcase))
-            (loop for sexp in result
-                  do (print sexp s)))))))
+  (if (not (and (pathname-name file) (str:ends-with? ".lisp" (file-namestring file))))
+    (return-from update-file))
+  (let ((result
+          (with-open-file (s file)
+            (loop for sexp = (macrolet ((:div (&rest rest)
+                                          (if (equal (subseq rest 0 5)
+                                                     '(:style
+                                                       "display: none;"
+                                                       :class
+                                                       "container-fluid my-tab position-absolute"
+                                                       :id))
+                                              ``(tab ,,(nth 5 rest)
+                                                 ,',@(subseq rest 6))
+                                              (print rest))))
+                               (print (read s nil)))
+                  while sexp
+                  collect sexp))))
+    (with-open-file (s file :direction :output :if-exists :supersede)
+      (let ((*print-case* :downcase))
+        (loop for sexp in result
+              do (print sexp s))))))
 ;;(mapc-directory-tree 'update-file
 ;;                     (asdf/system:system-source-directory :spickipedia)))))))
 (update-file #P"spickipedia/src/index.lisp")
-
-(macrolet ((:div (&rest rest)
-                 (format t "jo:~S~%" rest)
-                 (if (equal (ignore-errors (subseq rest 0 5))
-                            '(:style
-                              "display: none;"
-                              :class
-                              "container my-tab position-absolute"
-                              :id))
-                     ``(tab ,,(nth 5 rest)
-                        ,',@(subseq rest 6))
-                      ``(:div ,,@rest))))
-  (:div
-    (:div :style "display: none;" :class "container my-tab position-absolute"
-       :id "edit-quiz" `(:h1 :class "text-center" "Quiz ändern"))))
