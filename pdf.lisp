b9975822bf30986ae91153053d57bb735ed5fa86
mohe2015 <Moritz.Hedtke@t-online.de>
2019-07-15 22:49:13 +0200

    More implementation

--- pdf.lisp
+++ pdf.lisp
@@ -26,17 +26,44 @@
 
 ;; http://wwwimages.adobe.com/content/dam/acom/en/devnet/pdf/PDF32000_2008.pdf#page=135&zoom=120,-178,448
 
+;; read-object
+
 (defun read-until (test &optional (stream *standard-input*))
   (with-output-to-string (out)
     (loop for c = (peek-char nil stream nil nil)
        while (and c (not (funcall test c)))
        do (write-char (read-char stream) out))))
 
-(defun spacep (char)
-  (char= #\space char))
+(defun whitespace-char-p (x)
+  (or (char= #\space x)
+      (not (graphic-char-p x))
+      (char= #\[ x)))
 
-(with-input-from-string (in (get-decompressed))
-  (let ((e (read-until 'spacep in)))
-    (cond ((equal e "q") (print "push graphics"))
-	  ((equal e "Q") (print "pop graphics"))
-	  (t (print "unknown")))))
+(defun parse ()
+  (with-input-from-string (in (get-decompressed))
+    (let ((stack '()))
+      (loop
+	 (if (eq (peek-char nil in nil nil) #\[)
+	     (let ((*pdf-input-stream* in))
+	       (push (read-object in) stack)
+	       (format t "read-object ~a~%" stack))	 
+	     (let ((e (read-until 'whitespace-char-p in)))
+	       (break)
+	       (cond ((equal e "q") (format t "push graphics~%"))
+		     ((equal e "Q") (format t "pop graphics~%"))
+		     ((equal e "BT") (format t "begin text~%"))
+		     ((equal e "ET") (format t "end text~%"))
+		     ((equal e "Tf") (format t "font and size ~a~%" stack) (setf stack '())) ;; TODO the literal name belongs to it
+		     ((equal e "Tm") (format t "text matrix ~a~%" stack) (setf stack '()))
+		     ((equal e "cm") (format t "CTM ~a~%" stack) (setf stack '()))
+		     ((equal e "RG") (format t "stroking color ~a~%" stack) (setf stack '()))
+		     ((equal e "rg") (format t "non-stroking color ~a~%" stack) (setf stack '()))
+		     ((equal e "TJ") (format t "print text with positioning ~a~%" stack) (setf stack '()))
+		     ((equal e "TL") (format t "set text leading ~a~%" stack) (setf stack '()))
+		     ((equal e "T*") (format t "new line with leading ~a~%" stack) (setf stack '()))
+		     ((equal e "Td") (format t "new line with offset ~a~%" stack) (setf stack '()))
+		     ((equal e "gs") (format t "graphics state operator~%"))
+		     ((eq (elt e 0) #\/) (format t "literal name ~a~%" e))
+		     (t (push e stack) (format t "push-stack ~a~%" e)))
+	       (unless (read-char in nil) ;; TODO fixme array
+		 (return))))))))
