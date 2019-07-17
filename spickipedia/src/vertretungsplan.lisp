50178b360416bdb1dd2b49166b1e06697c671afa
mohe2015 <Moritz.Hedtke@t-online.de>
2019-07-17 22:32:52 +0200

    More implementing

--- spickipedia/src/vertretungsplan.lisp
+++ spickipedia/src/vertretungsplan.lisp
@@ -1,5 +1,5 @@
 (defpackage spickipedia.vertretungsplan
-  (:use :cl :spickipedia.pdf :spickipedia.libc :local-time))
+  (:use :cl :spickipedia.pdf :spickipedia.libc :local-time :str))
 (in-package :spickipedia.vertretungsplan)
 
 (defclass vertretungsplan ()
@@ -8,15 +8,40 @@
 (defun parse-vertretungsplan (file)
   (let ((extractor (parse file)))
     (read-newline extractor)
-    (format t "date ~a~%" (strptime (read-line-part extractor) "%a, %d. %b %Y %H:%M Uhr"))
+    (format t "updated ~a~%" (strptime (read-line-part extractor) "%a, %d. %b %Y %H:%M Uhr"))
     (read-newline extractor)
     (read-line-part extractor) ; date-code and school 
     (read-newline extractor)
-    (let ((for-header (read-line-part extractor)))
-      (cond ((equal for-header "Vertretungsplan für")
-	     (read-newline extractor)
-	     (let ((date (read-line-part extractor)))
-	       (format t "date ~a~%" (strptime date "%A, %d. %b %Y"))))
-	    (t (print for-header))))))
+    (let ((element (read-line-part extractor)))
+      (loop
+	 (cond ((equal element "Vertretungsplan für")
+		(read-newline extractor)
+		(let ((date (read-line-part extractor)))
+		  (format t "date ~a~%" (strptime date "%A, %d. %b %Y")))
+		(read-newline extractor)
+		(setf element (read-line-part extractor)))
+	       ((equal element "Aufsicht: v. d. Unterricht:")
+		(read-newline extractor)
+		(setf element (read-line-part extractor)))
+	       ((starts-with? "1. Pause:" (trim element))
+		(read-newline extractor)
+		(setf element (read-line-part extractor)))
+	       ((starts-with? "2. Pause:" (trim element))
+		(read-newline extractor)
+		(setf element (read-line-part extractor)))
+	       ((starts-with? "Bus:" (trim element))
+		(read-newline extractor)
+		(setf element (read-line-part extractor)))
+	       ((starts-with? "Reinigung:" (trim element))
+		(read-newline extractor)
+		(setf element (read-line-part extractor)))	
+	       ((starts-with? "fehlende Lehrer:" (trim element))
+		;; TODO read line
+		(setf element (read-line-part extractor)))
+			
+		
+	       
+	       ((not element) (error "unexpected end"))
+	       (t (format t "~a~%" element)))))))
 
  (parse-vertretungsplan #P"/home/moritz/Downloads/vs.pdf")
