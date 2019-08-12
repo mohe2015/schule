823148d6d72605b8e318dc6ee6296c6cdf444927
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2019-08-12 15:00:42 +0200

    Some improvements

--- spickipedia/js/substitution-schedule/index.lisp
+++ spickipedia/js/substitution-schedule/index.lisp
@@ -2,12 +2,20 @@
 
 (i "/js/show-tab.lisp" "showTab")
 (i "/js/cleanup.lisp" "cleanup")
-(i "/js/fetch.lisp" "checkStatus" "json" "html" "handleFetchError"
-   "cacheThenNetwork")
+(i "/js/fetch.lisp" "checkStatus" "json" "html" "handleFetchError" "cacheThenNetwork")
 (i "/js/utils.lisp" "all" "one" "clearChildren")
 (i "/js/template.lisp" "getTemplate")
 (i "/js/read-cookie.lisp" "readCookie")
 (i "/js/state-machine.lisp" "pushState")
+
+(defun group-by (xs key)
+  (chain
+   xs
+   (reduce
+    (lambda (rv x)
+      (chain (setf (getprop rv (getprop x key)) (or (getprop rv (getprop x key)) (array))) (push x))
+      rv)
+    (create))))
 
 (defroute "/substitution-schedule"
   (show-tab "#loading")
@@ -17,25 +25,14 @@
      (loop :for (k v) :of (chain data schedules) :do
 	  (let ((template (get-template "template-substitution-schedule")))
 	    (setf (inner-text (one ".substitution-schedule-date" template)) "1.1.2001")
-
-
-	    
-	    (let ((class-template (get-template "template-substitution-for-class")))
-	      (setf (inner-text (one ".template-class" class-template)) "clazz")
-	      (append template class-template))
-
-	    (group-by:group-by-repeated
-	     (chain v substitutions)
-	     :keys (list #'third)
-	     :tests (list #'string-equal))
-
-	    
-	    (loop for substitution in (chain v substitutions) do
-		 (chain console (log substitution)))
-
-	    (append (one "#substitution-schedule") template)
-	    
-	    (chain console (log k v))))
-     
-     (show-tab "#substitution-schedule")
-     
+	    (if (chain v substitutoins)
+		(loop :for (clazz substitutions) :of (group-by (chain v substitutions) "class") :do
+		     (let ((class-template (get-template "template-substitution-for-class")))
+		       (setf (inner-text (one ".template-class" class-template)) "clazz")
+		       (append template class-template))
+		     (loop for substitution in substitutions do
+			  (let ((substitution-template (get-template "template-substitution")))
+			    (setf (inner-text (one "li" substitution-template)) "substitution")
+			    (append class-template substitution-template)))))
+	    (append (one "#substitution-schedule") template)))
+     (show-tab "#substitution-schedule"))))
