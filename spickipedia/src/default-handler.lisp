ecf8f9b2f4929f08c9cd59108f1c8f74a93a9b0f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2019-08-18 17:20:46 +0200

    Try detecting subpaths

--- spickipedia/src/default-handler.lisp
+++ spickipedia/src/default-handler.lisp
@@ -9,3 +9,23 @@
                 (get-safe-mime-type path))
           path)
         (test))))
+
+;; (merge-pathnames #P"../subpath/" #P"/basepath/")
+
+;; https://stackoverflow.com/a/44684290/5074433
+(defun abspath (path-string)
+  (uiop:unix-namestring
+   (uiop:merge-pathnames*
+    (uiop:parse-unix-namestring path-string))))
+; (abspath "/basepath/../fakesubpath/")
+
+(defun subpath-p (base-path sub-path)
+  (let ((absolute-sub-directory (uiop:pathname-directory-pathname (abspath sub-path))))
+    (loop while (not (equal #P"/" absolute-sub-directory)) do
+	 (if (equal base-path absolute-sub-directory)
+	     (return-from subpath-p t))
+	 (setf absolute-sub-directory (uiop:pathname-parent-directory-pathname absolute-sub-directory)))
+  nil))
+
+(assert (subpath-p #P"/test/" #P"/test/subpath/"))
+(assert (not (subpath-p #P"/test/" #P"/test/../fakesubpath/")))
