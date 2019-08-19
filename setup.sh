a5da881697a6e4d3bde115214ba267de18015081
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2019-08-19 18:43:26 +0200

    Update popper.js

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+set -ex
+
 ln -sf $PWD/spickipedia/ ~/.roswell/local-projects/
 ln -sf $PWD/lack/ ~/.roswell/local-projects/
 ln -sf $PWD/parenscript/ ~/.roswell/local-projects/
@@ -6,8 +8,8 @@ ln -sf $PWD/mw-diff-sexp ~/.roswell/local-projects/
 ln -sf $PWD/cl-pdf ~/.roswell/local-projects/
 ln -sf $PWD/clack/ ~/.roswell/local-projects/
 
-(cd popper.js && npm install && npm run build)
+(cd popper.js && yarn install && yarn build)
 (cd bootstrap && npm install && npm run dist)
-cp popper.js/dist/umd/index.js spickipedia/static/popper.js
+cp popper.js/packages/popper/dist/umd/popper.js spickipedia/static/popper.js
 cp bootstrap/dist/js/bootstrap.js spickipedia/static/
 cp bootstrap/dist/css/bootstrap.css spickipedia/static/
