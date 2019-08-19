b4ee0efc091bdcc9f67551980b6ff11261084a9a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2019-08-19 21:59:25 +0200

    Fix setup

--- setup.sh
+++ setup.sh
@@ -10,6 +10,9 @@ ln -sf $PWD/clack/ ~/.roswell/local-projects/
 
 (cd popper.js && yarn install && yarn build)
 (cd bootstrap && npm install && npm run dist)
-cp popper.js/packages/popper/dist/umd/popper.js spickipedia/static/popper.js
+cp popper.js/packages/popper/dist/umd/popper.js spickipedia/static/
 cp bootstrap/dist/js/bootstrap.js spickipedia/static/
 cp bootstrap/dist/css/bootstrap.css spickipedia/static/
+cp popper.js/packages/popper/dist/umd/popper.js.map spickipedia/static/
+cp bootstrap/dist/js/bootstrap.js.map spickipedia/static/
+cp bootstrap/dist/css/bootstrap.css.map spickipedia/static/
