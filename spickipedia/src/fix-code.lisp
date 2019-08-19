2eea0adf2b08ef7926524c46b5509261a47f53af
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2019-08-19 16:17:34 +0200

    Fix it

--- spickipedia/src/fix-code.lisp
+++ spickipedia/src/fix-code.lisp
@@ -14,3 +14,12 @@
     (if result result code)))
 
 (fix-code '(if t (chain (one "#test") (modal "hide")) (progn)))
+
+(defun fix-file (file)
+  (with-open-file (in file)
+    (loop for sexp = (read-preserving-whitespace in nil) while sexp do
+	 (print sexp)
+	 (print (fix-code sexp)))))
+
+(loop for file in (directory "spickipedia/**/*.lisp") do
+     (fix-file file))
