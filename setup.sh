b28574ef72263716b1a3068e5568d998520e1b4d
mohe2015 <mohe2015@users.noreply.github.com>
2019-08-25 11:45:05 +0200

    Small bugfix

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 set -ex
 
-ln -sf $PWD/ ~/.roswell/local-projects/
-ln -sf $PWD/lack/ ~/.roswell/local-projects/
-ln -sf $PWD/parenscript/ ~/.roswell/local-projects/
-ln -sf $PWD/clack/ ~/.roswell/local-projects/
-ln -sf $PWD/mw-diff-sexp ~/.roswell/local-projects/
-ln -sf $PWD/cl-pdf ~/.roswell/local-projects/
-ln -sf $PWD/clack/ ~/.roswell/local-projects/
+ln -sf $PWD/ ~/quicklisp/local-projects/
+ln -sf $PWD/lack/ ~/quicklisp/local-projects/
+ln -sf $PWD/parenscript/ ~/quicklisp/local-projects/
+ln -sf $PWD/clack/ ~/quicklisp/local-projects/
+ln -sf $PWD/mw-diff-sexp ~/quicklisp/local-projects/
+ln -sf $PWD/cl-pdf ~/quicklisp/local-projects/
+ln -sf $PWD/clack/ ~/quicklisp/local-projects/
 
 (cd dependencies/popper.js && yarn install && yarn build)
 (cd dependencies/bootstrap && npm install && npm run dist)
+(cd dependencies/rust-web-push && ./setup.sh)
 cp dependencies/popper.js/packages/popper/dist/umd/popper.js static/
 cp dependencies/bootstrap/dist/js/bootstrap.js static/
 cp dependencies/bootstrap/dist/css/bootstrap.css static/
